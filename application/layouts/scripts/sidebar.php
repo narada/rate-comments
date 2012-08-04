b803375c613400e539b274238b477b70e8cae501
Shalinga <shalinga@venturit.com>
2012-08-04 20:56:47 +0530

    VT-ZEND layout.

--- application/layouts/scripts/sidebar.php
+++ application/layouts/scripts/sidebar.php
@@ -1,3 +1,8 @@
 <div id="sidebar" style="background-color: #EEEEEE; width: 150px;">
-
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Profile</a></li>
+		<li><a href="#">Create a post</a></li>
+		<li><a href="#">List of Post</a></li>
+	</ul>
 </div>
